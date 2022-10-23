d41d992490a235b181a2642bf3bc74611778fca5
ErfanAz82 <115965622+ErfanAz82@users.noreply.github.com>
2022-10-23 09:03:04 +0330

    Delete EX1_5.py

--- EX1_5.py
+++
@@ -1,9 +0,0 @@
-age = float(input("Please enter your age: "))
-if age <= 1:
-    print("You are an infant.")
-elif age < 13:
-    print("You are an child.")
-elif age < 20:
-    print("You are an teenager.")
-elif age >= 20:
-    print("You are an adult.")
