a68eab464af8e8b64d6f40aacdd0d507b57ab337
ErfanAz82 <115965622+ErfanAz82@users.noreply.github.com>
2022-10-25 16:39:12 +0330

    Add files via upload

---
+++ EX1_6.py
@@ -0,0 +1,23 @@
+Roman_Numerals = float(input("Please enter a number within 1 - 10: "))
+if Roman_Numerals == 1:
+    print("I")
+elif Roman_Numerals == 2:
+    print("II")
+elif Roman_Numerals == 3:
+    print("III")
+elif Roman_Numerals == 4:
+    print("IV")
+elif Roman_Numerals == 5:
+    print("V")
+elif Roman_Numerals == 6:
+    print("VI")
+elif Roman_Numerals == 7:
+    print("VII")
+elif Roman_Numerals == 8:
+    print("VIII")
+elif Roman_Numerals == 9:
+    print("IX")
+elif Roman_Numerals == 10:
+    print("X")
+else:
+    print("Wrong input! ")
