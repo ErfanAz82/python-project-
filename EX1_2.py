0faaf7a2091fb6d9f64d50a76baad7c2b81c2bce
ErfanAz82 <115965622+ErfanAz82@users.noreply.github.com>
2022-10-25 23:16:57 +0330

    Update EX1_2.py

--- EX1_2.py
+++ EX1_2.py
@@ -4,3 +4,6 @@ def triangle(base,height):
 base = int(input("Please enter the base: "))
 height = int(input("Please enter the height: "))
 print(f"Area of the triangle is {triangle(base,height)}")
+
+# int -> float
+# return (base * height) / 2 and calculate the area of triangle
